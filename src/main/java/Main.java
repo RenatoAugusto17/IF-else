1adbbaea75749e9feab3ef4609de77191e36abd9
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-09-02 12:23:37 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+System.out.println("Login:");
+  String login = sc.next();
+  int senhaAdmin = 1;
+    string login1 = "admin";
+
+    if ( login.equals("login1")) { System.out.println("Coloque sua senha:"); int senha = next.Int(); if(senha == senhaAdmin) {System.out.println("Acesso permitido");
+} else{System.out.println("Acesso negado!");}   
+ } else { System.out.println("login inválido");}
+
+
+sc.close();
+     }
+
+}
